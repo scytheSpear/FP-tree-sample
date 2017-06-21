7e202094ac809dfda60fce6e7bcc9a9c6fad9d69
scytheSpear <spear_id@outlook.com>
2017-06-21 16:28:29 +0800

    study sample

---
+++ src/fp/tree/sample/StrongAssociationRule.java
@@ -0,0 +1,11 @@
+package fp.tree.sample;
+
+
+import java.util.List;
+
+public class StrongAssociationRule {
+    public List<String> condition;
+    public String result;
+    public int support;
+    public double confidence;
+}
